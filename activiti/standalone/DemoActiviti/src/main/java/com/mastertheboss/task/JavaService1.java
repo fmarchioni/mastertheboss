71e40faec20ae72c5f869b5a5e7a84e87a3e01b1
francesco <fmarchio@redhat.com>
2016-08-28 22:19:10 +0200

    i.r.

---
+++ activiti/standalone/DemoActiviti/src/main/java/com/mastertheboss/task/JavaService1.java
@@ -0,0 +1,25 @@
+package com.mastertheboss.task;
+
+import org.activiti.engine.delegate.DelegateExecution;
+
+public class JavaService1 implements org.activiti.engine.delegate.JavaDelegate {
+
+	public void execute(DelegateExecution arg0) throws Exception {
+
+		if (isComplexTask()) {
+			System.out.println("This is a complex task!");
+			arg0.setVariable("result", "1");
+		}	
+		else  {
+			System.out.println("This is a simple task!");			
+		    arg0.setVariable("result", "0");
+	    }
+
+	}
+
+	private boolean isComplexTask() {
+
+		return Math.random() < 0.5;
+	}
+
+}
