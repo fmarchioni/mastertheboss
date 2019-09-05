2e8ed4021769af23c8fb4aba52b51c0e5e1ff537
francesco <francesco@fedora.station>
2019-09-05 21:05:39 +0200

    added new files

---
+++ flyway/quarkus/src/main/resources/db/migration/release_1/V1_1__create_account_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE account(
+   user_id VARCHAR PRIMARY KEY,
+   username VARCHAR (50) UNIQUE NOT NULL,
+   password VARCHAR (50) NOT NULL,
+   email VARCHAR (355) UNIQUE NOT NULL
+);
---
+++ flyway/quarkus/src/main/resources/db/migration/release_2/V1_2__insert_account_table.sql
@@ -0,0 +1,2 @@
+INSERT into account ( user_id, username, password, email) VALUES ('a1234567','frank','smith','frank@email.com');
+INSERT into account ( user_id, username, password, email) VALUES ('bc123454','mark','twain','mark@email.com');
