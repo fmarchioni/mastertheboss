29471e78cd9ecc1f98f09859c6c8ddbfb62058f0
francesco <francesco@fedora.station>
2019-09-22 10:59:20 +0200

    added archetype example

---
+++ maven-archetype/demo/demo-rest/src/main/java/com/example/HelloREST.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+@Path("/hello")
+public class HelloREST {
+
+    @GET
+    public Response hello(){
+
+        return Response
+                .ok( "Hello World")
+                .build();
+    }
+
+}
