0fd247ed4d591a363e65d7f401a5fa976ea8eee9
francesco <francesco@fedora.redhat.com>
2019-11-11 18:31:03 +0100

    fixed sql file

--- quarkus/quarkus-jdbc-realm/src/main/resources/import.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE test_user (
-  id INT,
-  username VARCHAR(255),
-  password VARCHAR(255),
-  role VARCHAR(255)
-);
-
-INSERT INTO test_user (id, username, password, role) VALUES (1, 'admin', 'admin', 'admin');
-INSERT INTO test_user (id, username, password, role) VALUES (2, 'user','user', 'user');
