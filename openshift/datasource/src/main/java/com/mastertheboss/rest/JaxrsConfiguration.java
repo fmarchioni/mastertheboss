16bc4b2955907166ac830498b277907ed6549d01
francesco <francesco@fedora.station>
2019-11-16 18:22:19 +0100

    commit

--- openshift/datasource/src/main/java/com/mastertheboss/rest/JaxrsConfiguration.java
+++ openshift/datasource/src/main/java/com/mastertheboss/rest/JaxrsConfiguration.java
@@ -3,6 +3,6 @@ package com.mastertheboss.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/rest")
 public class JaxrsConfiguration extends Application {
 }
