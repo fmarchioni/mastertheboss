68da577ca9361438360be539320595c8ef4e4725
francesco <francesco@fedora.station>
2019-11-30 10:31:04 +0100

    commit

--- hibernate/HibernateExample/src/main/java/com/mastertheboss/domain/Department.java
+++ hibernate/HibernateExample/src/main/java/com/mastertheboss/domain/Department.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class Department {
  
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
  
      
--- hibernate/HibernateExample/src/main/java/com/mastertheboss/domain/Employee.java
+++ hibernate/HibernateExample/src/main/java/com/mastertheboss/domain/Employee.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table
 public class Employee {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
  
     private String name;
