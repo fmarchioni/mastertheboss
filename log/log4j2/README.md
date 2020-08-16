c9025f6f0e2f6d1975078b5afc13344fbce51c53
fmarchio <francesco@fedora.station>
2020-08-16 13:13:28 +0200

    commit

---
+++ log/log4j2/README.md
@@ -0,0 +1,11 @@
+HelloWorld log4j example
+=====================================
+Example taken from: http://www.mastertheboss.com/jboss-server/jboss-log/using-log4j-with-jboss-as-7-and-wildfly
+
+This example demonstrates how to use Log4j in a Jakarta application.
+
+###### Deploy
+```shell
+mvn clean install wildfly:deploy
+```
+ 
---
+++ log/log4j2/src/main/java/com/mastertheboss/servlet/HelloWorldServlet.java
@@ -0,0 +1,41 @@
+package com.mastertheboss.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@WebServlet(name = "hello", urlPatterns = { "/hello" })
+public class HelloWorldServlet extends HttpServlet {
+
+
+	Logger logger = LogManager.getLogger(HelloWorldServlet.class);
+
+	public HelloWorldServlet() {
+		super();
+
+	}
+	
+	protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello World Servlet on WildFly</h1>");
+		logger.info("Hello world Log4j2 on WildFly");
+		logger.info("Request URI {} - Session Id {}.", request.getRequestURI(), request.getSession().getId());
+
+		writer.close();
+    }
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
