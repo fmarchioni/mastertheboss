2072e6edb668b556d5b596f3a63007c5c1109bc9
fmarchio <francesco@fedora.station>
2020-09-25 08:37:10 +0200

    example being changed

--- micro-services/mp-graphql/src/main/java/graphql/sample/client/WeatherApi.java
+++
@@ -1,9 +0,0 @@
-
-package graphql.sample.client;
-
-import org.eclipse.microprofile.graphql.Name;
-
-public interface WeatherApi {
-
-    TempAndPrecip currentConditions(@Name("location")String location);
-}
