d75844fbfff908b1f2606e10cf9a2fb3747dba5c
fmarchio <francesco@fedora.station>
2020-12-06 15:12:59 +0100

    commit

---
+++ maven-archetype/java-ee8/demo/demo-rest/src/main/java/com/example/AppConfig.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class AppConfig extends Application {
+}
---
+++ maven-archetype/java-ee8/src/main/resources/archetype-resources/src/main/java/AppConfig.java
@@ -0,0 +1,8 @@
+package ${package};
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class AppConfig extends Application {
+}
