efd7a66947a1806cff12706735de22eb42af062d
fmarchio <francesco@fedora.redhat.com>
2021-04-19 09:06:58 +0200

    added custom realm example

---
+++ security/ee-security-elytron/src/main/java/com/itbuzzpress/security/SecuredServlet.java
@@ -0,0 +1,47 @@
+ 
+package com.itbuzzpress.security;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+ 
+@SuppressWarnings("serial")
+@WebServlet("/secure")
+@ServletSecurity(@HttpConstraint(rolesAllowed = { "Admin" }))
+public class SecuredServlet extends HttpServlet {
+
+ 
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        Principal principal = null;
+        String authType = null;
+        String remoteUser = null;
+
+
+        principal = req.getUserPrincipal();
+
+        remoteUser = req.getRemoteUser();
+
+        authType = req.getAuthType();
+
+        writer.println("<html><head><title>servlet-security</title></head><body>");
+        writer.println("<h1>" + "Successfully called Secured Servlet " + "</h1>");
+        writer.println("<p>" + "Principal  : " + principal.getName() + "</p>");
+        writer.println("<p>" + "Remote User : " + remoteUser + "</p>");
+        writer.println("<p>" + "Authentication Type : " + authType + "</p>");
+        writer.println("</body></html>");
+        writer.close();
+    }
+
+}
