29e6d4d64614bebe6289097aa4eb84a275f1562d
fmarchio <francesco@fedora.redhat.com>
2021-06-10 16:09:02 +0200

    remove unneeded files

--- jbpm/simple-embedded-process/src/main/java/com/mastertheboss/util/EnvironmentProducer.java
+++ jbpm/simple-embedded-process/src/main/java/com/mastertheboss/util/EnvironmentProducer.java
@@ -1,61 +1,41 @@
 package com.mastertheboss.util;
-
-import java.util.List;
-
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.Produces;
+import javax.enterprise.util.AnnotationLiteral;
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
-
+import javax.enterprise.inject.spi.BeanManager;
+import org.kie.api.task.TaskService;
 import org.jbpm.services.api.DeploymentService;
 import org.jbpm.services.cdi.Kjar;
-import org.jbpm.services.cdi.Selectable;
-import org.jbpm.services.cdi.producer.UserGroupInfoProducer;
 import org.jbpm.services.task.audit.JPATaskLifeCycleEventListener;
-import org.kie.api.task.TaskLifeCycleEventListener;
-import org.kie.api.task.UserInfo;
-import org.kie.internal.identity.IdentityProvider;
-
+import org.jbpm.services.task.identity.JBossUserGroupCallbackImpl;
+import org.jbpm.services.task.lifecycle.listeners.TaskLifeCycleEventListener;
+import org.kie.internal.task.api.UserGroupCallback;
+@ApplicationScoped
 public class EnvironmentProducer {
-
-    @PersistenceUnit(unitName = "org.jbpm.domain")
-    private EntityManagerFactory emf;
-
-    @Inject
-  //  @Selectable
-    private UserGroupInfoProducer userGroupInfoProducer;
-
-    @Inject
-    @Kjar
-    private DeploymentService deploymentService;
-
-    @Produces
-    public EntityManagerFactory getEntityManagerFactory() {
-        return this.emf;
-    }
-
-    @Produces
-    public org.kie.api.task.UserGroupCallback produceSelectedUserGroupCalback() {
-        return userGroupInfoProducer.produceCallback();
-    }
-
-    @Produces
-    public UserInfo produceUserInfo() {
-        return userGroupInfoProducer.produceUserInfo();
-    }
-
-    @Produces
-    @Named("Logs")
-    public TaskLifeCycleEventListener produceTaskAuditListener() {
-        return new JPATaskLifeCycleEventListener(true);
-    }
-
-    @Produces
-    public DeploymentService getDeploymentService() {
-        return this.deploymentService;
-    }
-
+	 @Inject
+	    private BeanManager beanManager;
+	    private TaskService taskService;
+	    private EntityManagerFactory emf;
+	   @Produces    
+	    public UserGroupCallback produceSelectedUserGroupCalback() {
+	        return new JBossUserGroupCallbackImpl("classpath:/usergroup.properties");
+	    }
+	    
+	    @PersistenceUnit(unitName = "org.jbpm.sample")
+	    @ApplicationScoped
+	    @Produces
+	    public EntityManagerFactory getEntityManagerFactory() {
+	        if (this.emf == null) {
+	            this.emf = Persistence.createEntityManagerFactory("org.jbpm.sample");
+	        }
+	        return this.emf;
+	    } 
  
-   
+
 }
