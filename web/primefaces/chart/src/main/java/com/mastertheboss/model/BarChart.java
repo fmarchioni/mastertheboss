692f9af6b4e55fa135b8da147cdcc60c5f2d6ef8
fmarchio <francesco@fedora.redhat.com>
2021-09-13 19:36:52 +0200

    added chart demo

---
+++ web/primefaces/chart/src/main/java/com/mastertheboss/model/BarChart.java
@@ -0,0 +1,106 @@
+package com.mastertheboss.model;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+
+import org.primefaces.model.charts.*;
+import org.primefaces.model.charts.axes.cartesian.CartesianScales;
+import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearAxes;
+import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearTicks;
+import org.primefaces.model.charts.bar.*;
+import org.primefaces.model.charts.optionconfig.legend.Legend;
+import org.primefaces.model.charts.optionconfig.legend.LegendLabel;
+import org.primefaces.model.charts.optionconfig.title.Title;
+
+@Model
+public class BarChart {
+    private BarChartModel barModel;
+
+    @PostConstruct
+    public void init() {
+        barModel = new BarChartModel();
+        ChartData data = new ChartData();
+        
+        BarChartDataSet barDataSet = new BarChartDataSet();
+        barDataSet.setLabel("Acme Gross Incomes");
+        
+        List<Number> values = new ArrayList<>();
+        values.add(65);
+        values.add(59);
+        values.add(80);
+        values.add(81);
+        values.add(56);
+        values.add(55);
+        values.add(40);
+        barDataSet.setData(values);
+       
+        List<String> bgColor = new ArrayList<>();
+        bgColor.add("rgba(255, 99, 132, 0.2)");
+        bgColor.add("rgba(255, 159, 64, 0.2)");
+        bgColor.add("rgba(255, 205, 86, 0.2)");
+        bgColor.add("rgba(75, 192, 192, 0.2)");
+        bgColor.add("rgba(54, 162, 235, 0.2)");
+        bgColor.add("rgba(153, 102, 255, 0.2)");
+        bgColor.add("rgba(201, 203, 207, 0.2)");
+        barDataSet.setBackgroundColor(bgColor);
+        
+        List<String> borderColor = new ArrayList<>();
+        borderColor.add("rgb(255, 99, 132)");
+        borderColor.add("rgb(255, 159, 64)");
+        borderColor.add("rgb(255, 205, 86)");
+        borderColor.add("rgb(75, 192, 192)");
+        borderColor.add("rgb(54, 162, 235)");
+        borderColor.add("rgb(153, 102, 255)");
+        borderColor.add("rgb(201, 203, 207)");
+        barDataSet.setBorderColor(borderColor);
+        barDataSet.setBorderWidth(1);
+      
+        data.addChartDataSet(barDataSet);
+        
+        List<String> labels = new ArrayList<>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        labels.add("July");
+        data.setLabels(labels);
+
+        //Data
+        barModel.setData(data);
+        
+        //Options
+        BarChartOptions options = new BarChartOptions();
+        CartesianScales cScales = new CartesianScales();
+        CartesianLinearAxes linearAxes = new CartesianLinearAxes();
+        CartesianLinearTicks ticks = new CartesianLinearTicks();
+        ticks.setBeginAtZero(true);
+        linearAxes.setTicks(ticks);
+        cScales.addYAxesData(linearAxes);
+        options.setScales(cScales);
+        
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Bar Chart");
+        options.setTitle(title);
+
+        Legend legend = new Legend();
+        legend.setDisplay(true);
+        legend.setPosition("top");
+        LegendLabel legendLabels = new LegendLabel();
+        legendLabels.setFontStyle("bold");
+        legendLabels.setFontColor("#2980B9");
+        legendLabels.setFontSize(24);
+        legend.setLabels(legendLabels);
+        options.setLegend(legend);
+
+        barModel.setOptions(options);
+    }
+
+    public BarChartModel getBarModel() {
+        return barModel;
+    }
+}
---
+++ web/primefaces/chart/src/main/java/com/mastertheboss/model/LineChart.java
@@ -0,0 +1,63 @@
+package com.mastertheboss.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+
+import org.primefaces.model.charts.*;
+import org.primefaces.model.charts.line.*;
+import org.primefaces.model.charts.optionconfig.title.Title;
+
+@Model
+public class LineChart {
+    private LineChartModel lineModel;
+
+    @PostConstruct
+    public void init() {
+        lineModel = new LineChartModel();
+        ChartData data = new ChartData();
+        
+        LineChartDataSet dataSet = new LineChartDataSet();
+        List values = new ArrayList<>();
+        values.add(15);
+        values.add(2);
+        values.add(10);
+        values.add(25);
+        values.add(19);
+        values.add(23);
+ 
+        dataSet.setData(values);
+        dataSet.setFill(false);
+        dataSet.setLabel("Acme Gross Incomes");
+        dataSet.setBorderColor("rgb(75, 192, 192)");
+        dataSet.setLineTension(0.1);
+        data.addChartDataSet(dataSet);
+        
+        List<String> labels = new ArrayList<>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+ 
+        data.setLabels(labels);
+        
+        //Options
+        LineChartOptions options = new LineChartOptions();        
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Line Chart");
+        options.setTitle(title);
+        
+        lineModel.setOptions(options);
+        lineModel.setData(data);
+    }
+
+    public LineChartModel getLineModel() {
+        return lineModel;
+    }
+}
