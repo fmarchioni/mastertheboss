13dbdb72044e48afabd7c62367585032fe945fcf
fmarchio <francesco@fedora.redhat.com>
2021-09-13 12:41:25 +0200

    updated to wildfly 24

--- web/primefaces/export-datatable/src/main/java/com/mastertheboss/controller/PropertyManager.java
+++ web/primefaces/export-datatable/src/main/java/com/mastertheboss/controller/PropertyManager.java
@@ -2,6 +2,7 @@ package com.mastertheboss.controller;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -10,6 +11,8 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 
@@ -19,74 +22,83 @@ import com.mastertheboss.model.Person;
 @ViewScoped
 public class PropertyManager implements Serializable {
 
-    private String name;
-    private String surname;
-    private int age;
-    private String city;
-    // Getters/Setters omitted for brevity
+	private String name;
+	private String surname;
+	private int age;
+	private String city;
+	// Getters/Setters omitted for brevity
 
-    public void save() {
-        Person p = new Person(name, surname, age, city);
-        cacheList.add(p);
-    }
+	public void save() {
+		Person p = new Person(name, surname, age, city);
+		cacheList.add(p);
+	}
 
-    public void clear() {
-        cacheList.clear();
-    }
+	public void clear() {
+		cacheList.clear();
+	}
 
-    public void postProcessXLS(Object document) {
-        HSSFWorkbook wb = (HSSFWorkbook) document;
-        HSSFSheet sheet = wb.getSheetAt(0);
-        CellStyle style = wb.createCellStyle();
-        style.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
+	public void postProcessXLS(Object document) {
+		DataFormatter formatter = new DataFormatter(Locale.US);
+		HSSFWorkbook wb = (HSSFWorkbook) document;
+		HSSFSheet sheet = wb.getSheetAt(0);
+		CellStyle style = wb.createCellStyle();
+		style.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
 
-        for (Row row : sheet) {
-            for (Cell cell : row) {
-                cell.setCellValue(cell.getStringCellValue().toUpperCase());
-                cell.setCellStyle(style);
-            }
-        }
-    }
+		for (Row row : sheet) {
+			for (Cell cell : row) {
 
-    public ArrayList<Person> getCacheList() {
-        return cacheList;
-    }
+				switch (cell.getCellType()) {
 
-    public void setCacheList(ArrayList<Person> cacheList) {
-        this.cacheList = cacheList;
-    }
+				case STRING:
+					cell.setCellValue(cell.getStringCellValue().toUpperCase());
+					cell.setCellStyle(style);
+					break;
 
-    ArrayList<Person> cacheList = new ArrayList();
+				}
 
-    public String getName() {
-        return name;
-    }
+			}
+		}
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public ArrayList<Person> getCacheList() {
+		return cacheList;
+	}
 
-    public String getSurname() {
-        return surname;
-    }
+	public void setCacheList(ArrayList<Person> cacheList) {
+		this.cacheList = cacheList;
+	}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+	ArrayList<Person> cacheList = new ArrayList();
 
-    public int getAge() {
-        return age;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public String getSurname() {
+		return surname;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 }
