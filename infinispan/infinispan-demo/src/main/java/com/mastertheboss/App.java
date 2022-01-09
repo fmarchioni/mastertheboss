3ca97ecde5c0ae36a5f49942f3d6ca23e26b923d
fmarchio <francesco@fedora.station>
2022-01-09 11:34:22 +0100

    updated infinispan demo

--- infinispan/infinispan-demo/src/main/java/com/mastertheboss/App.java
+++ infinispan/infinispan-demo/src/main/java/com/mastertheboss/App.java
@@ -4,15 +4,22 @@ import org.infinispan.Cache;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
 
-public class App
+public class App 
 {
-
-    public static void main( String[] args ) throws Exception
+     
+    public static void main( String[] args )
     {
-        DefaultCacheManager cacheManager = new DefaultCacheManager("config.xml");
-        Cache cache = cacheManager.getCache();
-        cache.put("key", "value");
-        System.out.printf("=============> key = %s\n", cache.get("key"));
-        cacheManager.stop();
+          DefaultCacheManager cacheManager = new DefaultCacheManager();
+          // Define local cache configuration
+          cacheManager.defineConfiguration("local", new ConfigurationBuilder().build());
+          // Obtain the local cache
+          Cache<String, String> cache = cacheManager.getCache("local");
+          // Store a key
+          cache.put("name", "john");
+          // Retrieve the value for they key and print it out
+          System.out.printf("name = %s\n", cache.get("name"));
+          // Stop the cache manager and release all resources
+          cacheManager.stop();
+            
     }
 }
