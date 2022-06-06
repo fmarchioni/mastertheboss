406d3a6baacd10ca35fb91d3b400fc8d042e0afe
Francesco <marchioni.francesco@gmail.com>
2022-06-06 19:56:12 +0200

    update project

--- jax-rs/security-legacy/src/main/java/com/mastertheboss/jaxrs/service/SimpleRESTService.java
+++ jax-rs/security-legacy/src/main/java/com/mastertheboss/jaxrs/service/SimpleRESTService.java
@@ -4,11 +4,6 @@ package com.mastertheboss.jaxrs.service;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Path("/tutorial")
