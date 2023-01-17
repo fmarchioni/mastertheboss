9234df5bf344636bb661d96b44f28b7f6647d46c
Francesco <marchioni.francesco@gmail.com>
2023-01-17 16:12:44 +0100

    update project

--- log/log4j2/src/main/java/com/mastertheboss/servlet/HelloWorldServlet.java
+++ log/log4j2/src/main/java/com/mastertheboss/servlet/HelloWorldServlet.java
@@ -2,11 +2,11 @@ package com.mastertheboss.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,7 +20,7 @@ public class HelloWorldServlet extends HttpServlet {
 		super();
 
 	}
-	
+
 	protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
 
