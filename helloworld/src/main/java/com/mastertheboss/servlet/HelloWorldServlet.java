42fb40962e4d8b132747d9e7d840c16e3effb676
fmarchioni <marchioni.francesco@gmail.com>
2023-03-16 18:08:58 +0100

    update project

--- helloworld/src/main/java/com/mastertheboss/servlet/HelloWorldServlet.java
+++ helloworld/src/main/java/com/mastertheboss/servlet/HelloWorldServlet.java
@@ -2,11 +2,11 @@ package com.mastertheboss.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "hello", urlPatterns = { "/hello" })
 public class HelloWorldServlet extends HttpServlet {
