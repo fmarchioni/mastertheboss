53fd98e3ae3271b102a9fed1f470b47934ff2741
fmarchioni <marchioni.francesco@gmail.com>
2023-11-25 19:51:39 +0100

    update project

---
+++ web/primefaces/tabview/src/main/java/com/sample/bean/ItemView.java
@@ -0,0 +1,32 @@
+package com.sample.bean;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import org.primefaces.event.TabChangeEvent;
+import org.primefaces.event.TabCloseEvent;
+
+@Named
+@RequestScoped
+public class ItemView {
+
+   
+    public void onTabChange(TabChangeEvent event) {
+        FacesMessage msg = new FacesMessage("Tab Changed", "Active Tab: " + event.getTab().getTitle());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onTabClose(TabCloseEvent event) {
+        FacesMessage msg = new FacesMessage("Tab Closed", "Closed tab: " + event.getTab().getTitle());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+   
+
+    private void showMessage(String clientId) {
+        FacesContext.getCurrentInstance()
+                .addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_INFO, clientId + " multiview state has been cleared out", null));
+    }
+}
